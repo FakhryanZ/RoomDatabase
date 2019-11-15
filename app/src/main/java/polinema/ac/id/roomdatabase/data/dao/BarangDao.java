078881ff26a07b822a9bbe4ceaab6ce1f68719fa
Muhammad Fakhryan <fakhryanmuhammad@gmail.com>
2019-11-15 14:10:33 +0700

    add dao barangdao

---
+++ app/src/main/java/polinema/ac/id/roomdatabase/data/dao/BarangDao.java
@@ -0,0 +1,23 @@
+package polinema.ac.id.roomdatabase.data.dao;
+
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import polinema.ac.id.roomdatabase.data.entity.Barang;
+
+public interface BarangDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    Long insertBarang(Barang barang);
+
+    @Update
+    int updateBarang(Barang barang);
+
+    @Delete
+    int deleteBarang(Barang barang);
+
+    @Query("SELECT * FROM barangTable")
+    Barang[] selectALlBarangs();
+}
